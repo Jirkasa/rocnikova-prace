331c39aec8adb40ca04177b41fd5ce32913d4627
Jiří <satorajirka@seznam.cz>
2022-12-08 11:58:52 +0100

    add favicon

--- app/src/views/includes/page-start.php
+++ app/src/views/includes/page-start.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, maximum-scale=2.2, minimum-scale=1.0">
 
+    <link rel="icon" type="image/svg+xml" href="<?php echo URLROOT; ?>/img/logo.svg"/>
+
     <link rel="stylesheet" href="<?php echo ASSETSROOT; ?>/css/style.css">
 
     <title>
